e7cb228c754bd7693b28c74c240e4f3fa7dfc77a
Ruben Giosa <rubenalexgiosa@gmail.com>
2023-02-13 12:00:07 -0800

    WIP: code for for loop in order to concat all the csv files together

--- dsa-airflow/dags/rg_work.py
+++ dsa-airflow/dags/rg_work.py
@@ -1,4 +1,10 @@
 import pyspark
+import pandas as pd
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as sf      # sf = spark functions
+import pyspark.sql.types as st          # st = spark types
+
+
 sparkql = pyspark.sql.SparkSession.builder.master('local').getOrCreate()
 
 import os
@@ -10,6 +16,8 @@ dataframes = {}
 
 for csv in file_names:
     df = sparkql.read.csv(os.path.join(data_dir,csv+'.csv'), header=True)
-    dataframes[csv] = df
+    df = df.withColumn('stock_name', sf.lit(csv))
+    df.toDF('date', 'open', 'high', 'low', 'close', 'adj_close', 'volume', 'stock_name')
+    df.select('stock_name', 'Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume')
 
 print(dataframes)
