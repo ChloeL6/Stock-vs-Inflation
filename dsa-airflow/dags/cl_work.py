46d4ad3bb1c9749723a33e11b31bcd8869d15f69
ChloeL6 <yenchi.le1199@gmail.com>
2023-02-16 13:34:22 -0800

    redefine schema

--- dsa-airflow/dags/cl_work.py
+++ dsa-airflow/dags/cl_work.py
@@ -5,7 +5,7 @@ from airflow import DAG
 from airflow.decorators import dag,task
 from airflow.sensors.filesystem import FileSensor
 from airflow.hooks.filesystem import FSHook
-from collections import Counter
+
 
 import logging
 import sys
@@ -90,28 +90,17 @@ def get_client() -> bigquery.Client:
 
 # Define table schemas
 #avg cpi table
-CPI_METADATA = {
-    'cpi_rates': {
-        'table_name': 'cpi_rates',
-        'schema' : [
+CPI_RATES =[
             bigquery.SchemaField("year", "INTEGER", mode="REQUIRED"),
             bigquery.SchemaField("cpi", "FLOAT", mode="REQUIRED")]
-    }}
 
 #avg unemployment table
-UNEMP_METADATA = {
-    'unemployment_rates' :{
-        'table_name': 'unemployment_rates',
-        'schema': [
+UNEMPLOYMENT_RATES = [
             bigquery.SchemaField("year", "INTEGER", mode="REQUIRED"),
             bigquery.SchemaField("avg_unemp_per_year", "FLOAT", mode="REQUIRED")]
-    }}
 
 #avg stock table
-STOCK_METADATA = {
-    'avg_stock_price_per_year': {
-        'table_name': 'avg_stock_price_per_year',
-        'schema': [
+AVG_STOCK_PRICE_PER_YEAR = [
             bigquery.SchemaField("stock_name", "STRING", mode="REQUIRED"),
             bigquery.SchemaField("year", "INTEGER", mode="REQUIRED"),
             bigquery.SchemaField("open", "FLOAT", mode="REQUIRED"),
@@ -121,4 +110,67 @@ STOCK_METADATA = {
             bigquery.SchemaField("adj_close", "FLOAT", mode="REQUIRED"),
             bigquery.SchemaField("volume", "FLOAT", mode="REQUIRED"),
             bigquery.SchemaField("__index_level_0__", "FLOAT", mode="REQUIRED")]
-    }}
+
+# global dict to hold all tables schemas
+TABLE_SCHEMAS = {
+    'cpi_rates': 'CPI_RATES',
+    'unemployment_rates': 'UNEMPLOYMENT_RATES',
+    'avg_stock_price_per_year': 'AVG_STOCK_PRICE_PER_YEAR'
+}
+
+
+def create_table(table_name: str) -> None:
+    
+    assert table_name in TABLE_SCHEMAS, f"Table schema not found for table name: {table_name}"
+
+    client = get_client()
+    table_id = f"{PROJECT_NAME}.{DATASET_NAME}.{table_name}"
+
+    try:
+        table = client.get_table(table_id)      # table exists if this line doesn't raise exception
+        client.delete_table(table)
+        logger.info(f"dropped existed bigquery table: {table_id}")
+
+        # wait a couple seconds before creating the table again
+        time.sleep(2.0)
+    except NotFound:
+        # it's OK! table didn't exist
+        pass
+    # create the table
+    schema = TABLE_SCHEMAS[table_name]
+    table = bigquery.Table(table_id, schema=schema)
+    table = client.create_table(table, exists_ok=False)
+    logger.info(f"created bigquery table: {table_id}")
+
+
+def load_table() -> None:
+    
+    job_config = bigquery.LoadJobConfig(
+        df=pd.DataFrame, 
+        client=bigquery.Client, 
+        table_name=str, 
+        schema=bigquery.SchemaField,
+        create_disposition= 'CREATE_NEVER', 
+        write_disposition=str = 'WRITE_TRUNCATE',
+    )
+    
+    job = client.load_table_from_dataframe(df, destination=table_name, job_config=job_config)
+    job.result()        # wait for the job to finish
+        # get the number of rows inserted
+    table = client.get_table(table_id)
+    logger.info(f"inserted {table.num_rows} rows to {table_id}")
+
+
+
+
+def wait_for_file():
+    # define the file sensor...
+    # wait for the airports file in the "data_fs" filesystem connection
+    wait_for_file = FileSensor(
+        task_id='wait_for_file',
+        poke_interval=15,                   # check every 15 seconds
+        timeout=(30 * 60),                  # timeout after 30 minutes
+        mode='poke',                        # mode: poke, reschedule
+        filepath='/data'                    # file path to check (relative to fs_conn)
+        fs_conn_id='data_fs',               # file system connection (root path)
+    )
