96a5bd682189752a23839579a00df103afcd0166
Phil Kendall <philiprobertovich@gmail.com>
2023-02-22 02:33:15 -0800

    drop columns.

--- dsa-airflow/dags/phil_utils/transformations.py
+++ dsa-airflow/dags/phil_utils/transformations.py
@@ -11,4 +11,24 @@ DATASET_NAME = config['dataset']
 
 def tornados_transformations():
   # load tornadoes file
-  tornadoes_df = pd.read_csv()
+  tornadoes_df = pd.read_csv(os.path.join(DATA_DIR, config['tornadoes']))
+
+  # Drop columns
+  drop_cols = [
+    'stf',
+    'stn',
+    'ns',
+    'sn',
+    'sg',
+    'f1',
+    'f2',
+    'f3',
+    'f4',
+    'fc',
+  ]
+
+  tornadoes_df = tornadoes_df.drop(columns=drop_cols)
+
+  logger.info('Dropped columns.')
+
+  
